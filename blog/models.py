d40c014348942787344addd91731b8f26ec67c39
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-25 11:07:10 +0600

    article model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models  # noqa
+from .utils import BaseAttribute
 
-# Create your models here.
+
+class Article(BaseAttribute):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
