1537def0220503cfbe8ee650a2054986acd9ecee
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-25 15:51:24 +0600

    add token auth for articles

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import (
-    UserViewSet, CustomAuthToken, LogoutApiView, VideoLikeDislikeUsersAPIView
+    UserViewSet, CustomAuthToken, LogoutApiView,
 )
 
 # routers
