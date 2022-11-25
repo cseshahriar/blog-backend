2c64938032a70cc117f30e0c2bce44f1bd3ac996
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-25 15:03:43 +0600

    nested serializer for article

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .admin import Article
+from users.serializers import UserSerializer
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    created_user = UserSerializer(read_only=True)
 
     class Meta:
         model = Article
         fields = (
-            'id', 'title', 'description',
+            'id', 'title', 'description', 'created_user'
         )
