46d87cb98deb6c32cfcb4eeb373891391ecf8396
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-25 11:10:49 +0600

    article admin created

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin  # noqa
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_active', )
+    list_display_links = ('title', )
+    list_filter = ('is_active', 'created_user')
+    search_fields = ('title', )
