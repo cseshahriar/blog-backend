ddcb5aefbae5a82aadb6df905ea9d90dc5e01f3e
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-02 10:52:51 +0600

    article serializer add perform_create and perform_update

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,9 @@ class ArticleViewSets(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     authentication_classes = [TokenAuthentication, ]
+
+    def perform_create(self, serializer):
+        serializer.save(created_user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_user=self.request.user)
