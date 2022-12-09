3511e69b9ee019ed99669fa8ec4fb7cc764bd3b4
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-09 11:42:18 +0600

    blog admin created

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,109 @@
-from django.contrib import admin  # noqa
-from .models import Article
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import Category, Tag, Article, Comment
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'parent',
+        'title',
+        'slug',
+        'description',
+    )
+    list_filter = (
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'parent',
+    )
+    search_fields = ('slug',)
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'title',
+        'slug',
+        'description',
+    )
+    list_filter = (
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+    )
+    search_fields = ('slug',)
+    date_hierarchy = 'created_at'
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_active', )
-    list_display_links = ('title', )
-    list_filter = ('is_active', 'created_user')
-    search_fields = ('title', )
+    list_display = (
+        'id',
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'category',
+        'title',
+        'slug',
+        'description',
+        'thumbnail',
+        'meta_keywords',
+        'published_at',
+        'status',
+    )
+    list_filter = (
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'category',
+        'published_at',
+    )
+    raw_id_fields = ('tags',)
+    search_fields = ('slug',)
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'article',
+        'content',
+    )
+    list_filter = (
+        'created_user',
+        'updated_user',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'article',
+    )
+    date_hierarchy = 'created_at'
