625106a9ded173038e7d0f1822ef683713350eb2
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-09 15:21:24 +0600

    category, tag, comment viewsets

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include  # noqa
 from rest_framework import routers
-from .views import ArticleViewSets
+from .views import (
+    CategoryViewSets, TagViewSets, CommentViewSets, ArticleViewSets
+)
 
 router = routers.DefaultRouter()
+router.register('categories', CategoryViewSets)
+router.register('tags', TagViewSets)
+router.register('comments', CommentViewSets)
 router.register('articles', ArticleViewSets)
 
 urlpatterns = router.urls
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,51 @@
 from rest_framework import viewsets
-from .models import Article
-from .serializers import ArticleSerializer
+from .models import Category, Tag, Article, Comment
+from .serializers import (
+    CategorySerializer, TagSerializer, CommentSerializer, ArticleSerializer
+)
 from rest_framework.authentication import (
     TokenAuthentication,
 )
 from rest_framework.permissions import IsAuthenticated
+
+
+class CategoryViewSets(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(created_user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_user=self.request.user)
+
+
+class TagViewSets(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(created_user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_user=self.request.user)
+
+
+class CommentViewSets(viewsets.ModelViewSet):
+    queryset = Comment().objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(created_user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_user=self.request.user)
 
 
 class ArticleViewSets(viewsets.ModelViewSet):
